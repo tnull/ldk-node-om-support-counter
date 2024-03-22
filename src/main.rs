cda22f778d750b46c3e5e787e8a631355a9f875f
Elias Rohrer <dev@tnull.de>
2024-03-22 16:01:45 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use std::io;
+use std::io::prelude::*;
+use std::sync::Arc;
+use std::time::Duration;
+
+use ldk_node::{Builder, Config, LogLevel};
+
+use ldk_node::lightning::ln::msgs::SocketAddress;
+
+use ldk_node::bitcoin::Network;
+use ldk_node::bitcoin::secp256k1::PublicKey;
+
+fn main() {
+	let mut config = Config::default();
+	config.network = Network::Bitcoin;
+
+	let mut builder = Builder::from_config(config);
+	builder.set_storage_dir_path("/tmp/ldk_node_om_support/".to_string());
+	builder.set_log_level(LogLevel::Trace);
+
+	let node = Arc::new(builder.build().unwrap());
+	node.start().unwrap();
+
+	let event_node = Arc::clone(&node);
+	std::thread::spawn(move || loop {
+		event_node.wait_next_event();
+		event_node.event_handled();
+	});
+
+	println!("Node ID: {}", node.node_id());
+
+	let nodes_to_connect: Vec<(PublicKey, SocketAddress)> = vec![
+		("03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f".parse().unwrap(), "3.33.236.230:9735".parse().unwrap()),
+		("0217890e3aad8d35bc054f43acc00084b25229ecff0ab68debd82883ad65ee8266".parse().unwrap(), "23.237.77.11:9735".parse().unwrap()),
+		("035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226".parse().unwrap(), "170.75.163.209:9735".parse().unwrap()),
+		("02f1a8c87607f415c8f22c00593002775941dea48869ce23096af27b0cfdcc0b69".parse().unwrap(), "52.13.118.208:9735".parse().unwrap()),
+		("0364913d18a19c671bb36dd04d6ad5be0fe8f2894314c36a9db3f03c2d414907e1".parse().unwrap(), "192.243.215.102:9735".parse().unwrap()),
+		("02cfdc6b60e5931d174a342b20b50d6a2a17c6e4ef8e077ea54069a3541ad50eb0".parse().unwrap(), "52.89.237.109:9735".parse().unwrap()),
+		("024bfaf0cabe7f874fd33ebf7c6f4e5385971fc504ef3f492432e9e3ec77e1b5cf".parse().unwrap(), "52.1.72.207:9735".parse().unwrap()),
+		("037659a0ac8eb3b8d0a720114efc861d3a940382dcfa1403746b4f8f6b2e8810ba".parse().unwrap(), "34.78.139.195:9735".parse().unwrap()),
+		("03cde60a6323f7122d5178255766e38114b4722ede08f7c9e0c5df9b912cc201d6".parse().unwrap(), "34.65.85.39:9745".parse().unwrap()),
+		("0294ac3e099def03c12a37e30fe5364b1223fd60069869142ef96580c8439c2e0a".parse().unwrap(), "8.210.134.135:26658".parse().unwrap()),
+		("03037dc08e9ac63b82581f79b662a4d0ceca8a8ca162b1af3551595b8f2d97b70a".parse().unwrap(), "34.68.41.206:9735".parse().unwrap()),
+		("026165850492521f4ac8abd9bd8088123446d126f648ca35e60f88177dc149ceb2".parse().unwrap(), "45.86.229.190:9735".parse().unwrap()),
+		("03c2abfa93eacec04721c019644584424aab2ba4dff3ac9bdab4e9c97007491dda".parse().unwrap(), "13.113.39.53:9735".parse().unwrap()),
+		("030c3f19d742ca294a55c00376b3b355c3c90d61c6b6b39554dbc7ac19b141c14f".parse().unwrap(), "54.77.250.40:9735".parse().unwrap()),
+		("034ea80f8b148c750463546bd999bf7321a0e6dfc60aaf84bd0400a2e8d376c0d5".parse().unwrap(), "213.174.156.66:9735".parse().unwrap()),
+		("02e4971e61a3f55718ae31e2eed19aaf2e32caf3eb5ef5ff03e01aa3ada8907e78".parse().unwrap(), "52.38.27.190:9735".parse().unwrap()),
+	];
+
+	for (node_id, address) in nodes_to_connect {
+		match node.connect(node_id, address.clone(), false) {
+			Ok(()) => {},
+			Err(e) => {
+				eprintln!("Failed to connect to node {}@{}: {}", node_id, address, e);
+			}
+		}
+	}
+
+std::thread::sleep(Duration::from_secs(10));
+
+	let count_node = Arc::clone(&node);
+	std::thread::spawn(move || loop {
+		let (num_om_support, num_nodes) = count_node.network_onion_message_support();
+		let share = num_om_support as f32 / num_nodes as f32;
+		println!("Nodes that announce OM support: {}/{} = {}", num_om_support, num_nodes, share);
+		std::thread::sleep(Duration::from_secs(10));
+	});
+
+	pause();
+	node.stop().unwrap();
+}
+
+fn pause() {
+	let mut stdin = io::stdin();
+	let mut stdout = io::stdout();
+
+	// We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+	write!(stdout, "Press any key to continue...").unwrap();
+	stdout.flush().unwrap();
+
+	// Read a single byte and discard
+	let _ = stdin.read(&mut [0u8]).unwrap();
+}
